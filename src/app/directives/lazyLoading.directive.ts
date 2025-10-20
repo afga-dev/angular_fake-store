a914ed3f90b0f64a0254000b6873ba50fdc91802
Ana Guerra <afga.work.contact@gmail.com>
2025-10-20 14:39:36 -0600

    feat(shop): add lazy loading for products

---
+++ src/app/directives/lazyLoading.directive.ts
@@ -0,0 +1,48 @@
+import { Directive, ElementRef, Input, OnInit, Renderer2 } from '@angular/core';
+
+@Directive({
+  selector: '[appLazyLoading]',
+  standalone: true,
+})
+export class LazyLoadingDirective implements OnInit {
+  @Input() appLazyLoading!: string;
+
+  private intersectionObserver?: IntersectionObserver;
+
+  constructor(
+    private element: ElementRef<HTMLImageElement>,
+    private renderer: Renderer2
+  ) {}
+
+  ngOnInit(): void {
+    const image = this.element.nativeElement;
+
+    this.renderer.addClass(image, 'loading');
+
+    if ('IntersectionObserver' in window) {
+      this.intersectionObserver = new IntersectionObserver((entries) => {
+        entries.forEach((entry) => {
+          if (entry.isIntersecting) {
+            this.loadImage(image);
+            this.intersectionObserver?.unobserve(image);
+          }
+        });
+      });
+      this.intersectionObserver.observe(image);
+    } else {
+      this.loadImage(image);
+    }
+  }
+
+  private loadImage(image: HTMLImageElement) {
+    this.renderer.addClass(image, 'loading');
+
+    const temporal = new Image();
+    temporal.src = this.appLazyLoading;
+    temporal.onload = () => {
+      this.renderer.setAttribute(image, 'src', this.appLazyLoading);
+      this.renderer.removeClass(image, 'loading');
+      this.renderer.addClass(image, 'loaded');
+    };
+  }
+}
