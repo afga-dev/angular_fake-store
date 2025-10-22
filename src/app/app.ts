bacefd1c79981ecce8cddd9fda4cc32d7193c6db
Ana Guerra <afga.work.contact@gmail.com>
2025-10-22 16:24:11 -0600

    refactor(inputs): update Input/Output usage for Angular 19

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,22 +1,13 @@
 import { Component, inject, OnInit } from '@angular/core';
-import { Router, RouterModule, RouterOutlet } from '@angular/router';
+import { Router, RouterOutlet } from '@angular/router';
 import { UserService } from './services/user.service';
-import { CommonModule } from '@angular/common';
-import { FormsModule } from '@angular/forms';
 import { SearchComponent } from './pages/search.component/search.component';
 import { NavbarComponent } from './pages/navbar.component/navbar.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [
-    CommonModule,
-    FormsModule,
-    RouterModule,
-    RouterOutlet,
-    SearchComponent,
-    NavbarComponent,
-  ],
+  imports: [RouterOutlet, SearchComponent, NavbarComponent],
   templateUrl: './app.html',
   styleUrl: './app.css',
 })
@@ -41,10 +32,5 @@ export class App implements OnInit {
 
   closeSearch(): void {
     this.showSearch = false;
-  }
-
-  onSignOut(): void {
-    this.userService.onSignOut();
-    this.router.navigateByUrl('/signin');
   }
 }
--- src/app/pages/search.component/search.component.ts
+++ src/app/pages/search.component/search.component.ts
@@ -1,12 +1,4 @@
-import {
-  AfterViewInit,
-  Component,
-  ElementRef,
-  EventEmitter,
-  Input,
-  Output,
-  ViewChild,
-} from '@angular/core';
+import { Component, ElementRef, input, output, ViewChild } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 
 @Component({
@@ -16,30 +8,21 @@ import { FormsModule } from '@angular/forms';
   templateUrl: './search.component.html',
   styleUrl: './search.component.css',
 })
-export class SearchComponent implements AfterViewInit {
+export class SearchComponent {
   searchTerm = '';
 
-  @Input() isOpen = false;
-  @Output() close = new EventEmitter<void>();
-  @Output() search = new EventEmitter<string>();
+  isSearchOpen = input(false);
+  closeSearch = output<void>();
+  search = output<string>();
 
   @ViewChild('searchInput') private searchInput?: ElementRef<HTMLInputElement>;
 
-  ngAfterViewInit(): void {
-    if (this.isOpen) this.focusInput();
-  }
-
   ngOnChanges(): void {
-    if (this.isOpen) this.focusInput();
+    if (this.isSearchOpen()) this.focusInput();
   }
 
   focusInput() {
     setTimeout(() => this.searchInput?.nativeElement.focus(), 300);
-  }
-
-  onClose() {
-    this.isOpen = false;
-    this.close.emit();
   }
 
   onSearch() {
@@ -48,6 +31,6 @@ export class SearchComponent implements AfterViewInit {
 
     this.search.emit(query);
     this.searchTerm = '';
-    this.onClose();
+    this.closeSearch.emit();
   }
 }
